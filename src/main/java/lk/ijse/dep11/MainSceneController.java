a1621c6a7738b20829ed9342a90427fae6a25350
Shehan Rathnayake <shehandj.swd@gmail.com>
2023-08-29 08:42:19 +0530

    Basic UI components added

--- src/main/java/lk/ijse/dep11/MainSceneController.java
+++ src/main/java/lk/ijse/dep11/MainSceneController.java
@@ -1,4 +1,40 @@
 package lk.ijse.dep11;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.layout.AnchorPane;
+
 public class MainSceneController {
+    public AnchorPane root;
+    public AnchorPane panelRoot;
+    public Button btnAdd;
+    public Button btnPlaylist;
+    public Button btnPlay;
+    public Label lblDisplay;
+    public Button btnBack;
+    public Button brnForward;
+    public ProgressBar pbTimeline;
+    public Label lblEndTime;
+    public Label lblStartTime;
+    public Button btnStop;
+
+    public void btnAddOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnPlaylistOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnPlayOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnBackOnAction(ActionEvent actionEvent) {
+    }
+
+    public void brnForwardOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnStopOnAction(ActionEvent actionEvent) {
+    }
 }
